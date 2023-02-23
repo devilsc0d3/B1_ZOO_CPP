2cafc11066e722a9d59485b3793d3499dfcfcf41
fleo <leo.faure@ynov.com>
2023-02-23 22:51:14 +0100

    add class market

--- Market.h
+++ Market.h
@@ -4,10 +4,33 @@
 
 #ifndef B1_CPP_ZOO_MARKET_H
 #define B1_CPP_ZOO_MARKET_H
+#include <iostream>
+
+using namespace  std;
 
 
 class Market {
-
+public :
+    void AddAnimal(string Species, int type) {
+        //add animal
+        //less money
+    };
+    void AddFood(string type) {
+        //add food
+        //less money
+    };
+    void AddEnclosure(string Species) {
+        //add enclosure
+        //less money
+    };
+    void SellAnimal(string Species) {
+        //add money
+        //less animal
+    };
+    void SellEnclosure(string Species) {
+        //add money
+        //less enclosure
+    };
 };
 
 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Market.h"
+#include "Zoo.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
