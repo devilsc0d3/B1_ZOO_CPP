38cee84e4b8f314b4d3f51bb28dafc4008e4c420
Brun <sasha.brun@ynov.com>
2023-02-27 14:17:28 +0100

    Add Menu

---
+++ Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Utilisateur on 27/02/2023.
+//
+
+#include "Menu.h"
---
+++ Menu.h
@@ -0,0 +1,14 @@
+//
+// Created by Utilisateur on 27/02/2023.
+//
+
+#ifndef B1_CPP_ZOO_MENU_H
+#define B1_CPP_ZOO_MENU_H
+
+
+class Menu {
+
+};
+
+
+#endif //B1_CPP_ZOO_MENU_H
