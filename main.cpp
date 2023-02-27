b40ab71325a90ab7fa9185de0fe323d6193a92b4
Brun <sasha.brun@ynov.com>
2023-02-27 14:49:04 +0100

    Modif Menu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include "Market.h"
 #include "Zoo.h"
+#include "Menu.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+
+    Menu menu;
+    menu.show();
+
+    return 0;
+
     return 0;
 }
