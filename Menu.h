bbec0b2820605c509a930324cc5f806924056350
Brun <sasha.brun@ynov.com>
2023-03-19 16:42:44 +0100

    refresh add

--- Menu.h
+++ Menu.h
@@ -45,6 +45,7 @@ public:
         } else if (choice == 2) {
             cout << "You have chosen to skip the month." << endl;
             zoo->subvention();
+            zoo->RefreshVisitor();
             zoo->VisitorforAnimals();
             zoo->eventExceptional();
             time->skip();
