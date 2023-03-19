82e91c5bff45114dc9642c158aa6fb5667852032
Brun <sasha.brun@ynov.com>
2023-03-19 16:48:02 +0100

    add days

--- Time.h
+++ Time.h
@@ -9,22 +9,33 @@ using namespace std;
 class Time {
     int year;
     int month;
+    int day;
 
 public:
-    Time() : year(0), month(1) {}
+    Time() : year(0), month(1), day(0) {}
 
     void skip() {
         month = (month % 12) + 1;
         if (month == 1) {
             year += 1;
         }
+    }
 
+    void skipDay(){
+        day = (day % 31) + 1;
+        if (day == 1) {
+            month += 1;
+        }
     }
 
     string toString() const {
         static const string months[] = {"January", "February", "March", "April", "May", "June", "July",
                                         "August", "September", "October", "November", "December"};
         return months[month - 1] + " " + to_string(year);
+    }
+
+    int getDay() {
+        return day;
     }
 
     int getMonth() {
